b5fcc5d33c5c2da8f4f569c3d215e56beb41152b
Sathvik-R <sathvikrk.ds@gmail.com>
2025-01-14 14:45:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Code Analysis"    ,
+    version="0.0.1",
+    author="Sathvik",
+    author_email="sathvikrk.ds@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
